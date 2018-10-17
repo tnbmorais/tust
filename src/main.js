6940070f8009f32c58472027feac2a62d372c397
Tiago Morais <tnbmorais@gmail.com>
2018-10-17 22:57:00 +0100

    Add Muse UI dependency

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
 import App from './App';
 
 Vue.config.productionTip = false;
+
+Vue.use(MuseUI);
 
 /* eslint-disable no-new */
 new Vue({
