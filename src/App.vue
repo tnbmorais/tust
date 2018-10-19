db126f3df7479c08c05650fa23df27eb20645ea5
Tiago Morais <tiago.morais@blip.pt>
2018-10-19 18:07:47 +0100

    Add template base styles

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="main">
-        <router-link to="/">
-            <mu-avatar class="logo" size="126" color="blue" text-color="yellow">TUST</mu-avatar>
-        </router-link>
+        <header class="header">
+            <router-link to="/">
+                <mu-avatar class="logo" size="126" color="blue" text-color="yellow">TUST</mu-avatar>
+            </router-link>
+        </header>
         <router-view class="content"></router-view>
-        <mu-divider class="bottom-divider"></mu-divider>
-        <mu-bottom-nav class="bottom-navigation" v-bind:value="routeSelected">
-            <mu-bottom-nav-item
-                title="Planear Viagem"
-                to="/"
-                value="/"
-                icon="directions_bus"></mu-bottom-nav-item>
-            <mu-bottom-nav-item
-                title="Mapa"
-                to="/city-map"
-                value="/city-map"
-                icon="location_on"></mu-bottom-nav-item>
-        </mu-bottom-nav>
+        <footer class="footer">
+            <mu-divider class="bottom-divider"></mu-divider>
+            <mu-bottom-nav class="bottom-navigation" v-bind:value="routeSelected">
+                <mu-bottom-nav-item
+                    title="Planear Viagem"
+                    to="/"
+                    value="/"
+                    icon="directions_bus"></mu-bottom-nav-item>
+                <mu-bottom-nav-item
+                    title="Mapa"
+                    to="/city-map"
+                    value="/city-map"
+                    icon="location_on"></mu-bottom-nav-item>
+            </mu-bottom-nav>
+        </footer>
     </div>
 </template>
 
@@ -36,29 +40,37 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+body {
+    background-color: white;
+    width: 100%;
+    height: 100%;
+}
+
 .main {
+    width: 100%;
+    height: 100%;
     text-align: center;
-    padding-top: 20px;
 }
 
-.logo {
-    font-size: 40px !important;
-}
-
-.bottom-divider {
-    position: fixed;
+.header {
     width: 100%;
-    bottom: 56px;
-}
+    margin-top: 20px;
 
-.bottom-navigation {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
+    .logo {
+        font-size: 40px !important;
+    }
 }
 
 .content {
+    width: 100%;
     padding-bottom: 56px;
+}
+
+.footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    z-index: 1;
 }
 </style>
