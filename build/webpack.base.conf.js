0d6731cca50343f0e785f828e3f7c65a82320c82
Tiago Morais <tnbmorais@gmail.com>
2018-10-22 00:15:16 +0100

    Add manifest to app

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,8 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const manifest = require('../manifest.json');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -88,5 +90,8 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
-  }
+  },
+  plugins: [
+    new WebpackPwaManifest(manifest)
+  ]
 }
